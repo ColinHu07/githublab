f3e7a3b25cbffc4d60d12bad3fc9e2142b0571a3
Colin Hu <colinhu@MacBook-Pro.fios-router.home>
2022-12-05 21:17:51 -0500

    Merged

--- Student.java
+++ Student.java
@@ -1,16 +1,20 @@
 public class Student {
     String name;
+    String email;
+    int grade;
 
-    public Student(String n) {
+    public Student(String n, String e) {
         name = n;
+        email = e; 
+        grade g;
     }
 
     public void printStudent() {
-        System.out.println(name);
+        System.out.println(name + " " + email + " " +grade);
     }
 
     public static void main(String[] args) {
-        Student b = new Student("Jhon Doe");
-        b.printStudent();
+        Student b = new Student("John Doe", e: "john@email.com", g:10);
+        b.printStudent(); 
     }
-}
+} 
